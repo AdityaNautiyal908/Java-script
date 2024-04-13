2d27440594bf5f15553310977f761a9592391c6a
AdityaNautiyal908 <nautiyaladitya7@gmail.com>
2024-04-13 09:02:51 +0530

    Add

--- Operators and Conditional Statement/Hello.js
+++ Operators and Conditional Statement/Hello.js
@@ -1,4 +1,4 @@
-// Artimetic Operators
+// // Artimetic Operators
 let a = 5;
 let b = 2;
 
@@ -8,7 +8,7 @@ console.log("a-b = ",a-b);
 console.log("a*b = ",a*b);
 console.log("a/b = ",a/b);
 
-// Q1. Get user to input a number using prompt("Enter a number : ").Check if the number is a mutiple of 5 or not.
+// // Q1. Get user to input a number using prompt("Enter a number : ").Check if the number is a mutiple of 5 or not.
 
 let num;
 num = prompt("Enter a number")
