c341f817eab70c48eed3fbf68e4b6a68faf2d8ac
AdityaNautiyal908 <nautiyaladitya7@gmail.com>
2024-04-15 23:22:56 +0530

    add

--- Loops and String/index.js
+++ Loops and String/index.js
@@ -1,9 +1,10 @@
-//for-of loop
+//Number guessing game
+let gameNum = 25;
 
-let str = "ApnaCollage";
-let size = 0;
-for(let i of str){
-    console.log("i=",i);
-    size++;
+let userNum = prompt("Guess the game number : ");
+console.log(userNum);
+
+while(userNum!=gameNum){
+    userNum = prompt("You entered wrong number. Guess again : ");
 }
-console.log("string size = ",size);
+console.log("Congratulations, you entered the right number");
