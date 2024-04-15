14b17f74758bb96aa072ccb4a2776cd4a6fcec53
AdityaNautiyal908 <nautiyaladitya7@gmail.com>
2024-04-15 18:36:36 +0530

    Add

--- DOM/script.js
+++ DOM/script.js
@@ -1,2 +1,12 @@
-let elements = document.querySelector("p");
-console.dir(elements);
+// let h2 = document.querySelector("h2");
+// console.dir(h2.innerText);
+
+// h2.innerText = h2.innerText + " from Apna Collage Student";
+
+let divs = document.querySelectorAll(".box");
+for(div of divs){
+    div.innerText = "new unique value "
+}
+divs[0].innerText = "new unique value 1";
+divs[1].innerText = "new unique value 2";
+divs[2].innerText = "new unique value 3";
