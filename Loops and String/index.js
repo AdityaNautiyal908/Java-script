ccb8d1cab61970e7041e3a3f96fe1b278a723239
AdityaNautiyal908 <nautiyaladitya7@gmail.com>
2024-04-16 18:28:48 +0530

    Add

--- Loops and String/index.js
+++ Loops and String/index.js
@@ -1,20 +1,20 @@
-//Number guessing game
-// let gameNum = 25;
+// Number guessing game
+let gameNum = 25;
 
-// let userNum = prompt("Guess the game number : ");
-// console.log(userNum);
+let userNum = prompt("Guess the game number : ");
+console.log(userNum);
 
-// while(userNum!=gameNum){
-//     userNum = prompt("You entered wrong number. Guess again : ");
-// }
-// console.log("Congratulations, you entered the right number");
+while(userNum!=gameNum){
+    userNum = prompt("You entered wrong number. Guess again : ");
+}
+console.log("Congratulations, you entered the right number");
 
-// let str = "Aditya";
-// console.log(str[1]);
+let str = "Aditya";
+console.log(str[1]);
 
-//Template Literals
-// let specialString = `This is a template literals`
-// console.log(typeof specialString);
+// Template Literals
+let specialString = `This is a template literals`
+console.log(typeof specialString);
 
 let obj = {
     item : "pen",
@@ -26,3 +26,7 @@ let output = `the cost of ${obj.item} is ${obj.price} rupees`;
 console.log(output);
 
 console.log("The cost of ",obj.item,"is",obj.price,"rupees");
+
+let fullName = prompt("enter your full name without spaces");
+let username = "@" + fullName + fullName.length;
+console.log(username);
